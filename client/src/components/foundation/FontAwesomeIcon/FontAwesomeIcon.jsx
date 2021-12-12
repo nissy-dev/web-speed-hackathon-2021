6208b05581f07cebb4bd866ff1f92f5041de87e2
nissy-dev <nd.12021218@gmail.com>
2021-12-12 18:18:10 +0900

    stop downloading svg icon

--- client/src/components/foundation/FontAwesomeIcon/FontAwesomeIcon.jsx
+++ client/src/components/foundation/FontAwesomeIcon/FontAwesomeIcon.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-/**
- * @typedef {object} Props
- * @property {string} iconType
- * @property {'solid' | 'regular'} styleType
- */
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons/faSignInAlt';
+import { faBalanceScale } from '@fortawesome/free-solid-svg-icons/faBalanceScale';
 
-/** @type {React.VFC<Props>} */
-const FontAwesomeIcon = ({ iconType, styleType }) => {
-  return (
-    <svg className="font-awesome inline-block leading-none fill-current">
-      <use xlinkHref={`/sprites/font-awesome/${styleType}.svg#${iconType}`} />
-    </svg>
-  );
+import { faPause } from '@fortawesome/free-solid-svg-icons/faPause';
+import { faPlay } from '@fortawesome/free-solid-svg-icons/faPlay';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons/faCircleNotch';
+import { faImages } from '@fortawesome/free-solid-svg-icons/faImages';
+import { faMusic } from '@fortawesome/free-solid-svg-icons/faMusic';
+import { faVideo } from '@fortawesome/free-solid-svg-icons/faVideo';
+
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons/faCalendarAlt';
+
+export {
+  FontAwesomeIcon,
+  faHome,
+  faEdit,
+  faUser,
+  faSignInAlt,
+  faBalanceScale,
+  faPause,
+  faPlay,
+  faExclamationCircle,
+  faCircleNotch,
+  faImages,
+  faMusic,
+  faVideo,
+  faCalendarAlt,
 };
-
-export { FontAwesomeIcon };
