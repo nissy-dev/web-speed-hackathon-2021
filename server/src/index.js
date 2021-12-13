ac2ff750080055722d07806a9a3e2be057b15437
nissy-dev <nd.12021218@gmail.com>
2021-12-13 23:38:18 +0900

    http2対応

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,16 @@
-import http from 'http';
+import fs from 'fs';
+import http2 from 'http2';
 
 import { app } from './app';
 import { insertSeeds } from './seeds';
 import { sequelize } from './sequelize';
 
 async function main() {
-  const server = http.createServer(app);
+  const options = {
+    key: fs.readFileSync('./keys/localhost-key.pem'),
+    cert: fs.readFileSync('./keys/localhost.pem'),
+  };
+  const server = http2.createServer(options, app);
 
   // データベースの初期化をします
   await sequelize.sync({
