ba702d0592974b0dccff4ec3d7b82a44aa136972
nissy-dev <nd.12021218@gmail.com>
2021-12-23 00:32:26 +0900

    wip

--- client/src/containers/AppContainer/AppContainer.jsx
+++ client/src/containers/AppContainer/AppContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Helmet } from 'react-helmet';
 import { Route, Routes, useLocation } from 'react-router-dom';
 
@@ -7,11 +7,18 @@ import { useFetch } from '../../hooks/use_fetch';
 import { fetchJSON } from '../../utils/fetchers';
 import { AuthModalContainer } from '../AuthModalContainer';
 import { NewPostModalContainer } from '../NewPostModalContainer';
-import { NotFoundContainer } from '../NotFoundContainer';
-import { PostContainer } from '../PostContainer';
-import { TermContainer } from '../TermContainer';
-import { TimelineContainer } from '../TimelineContainer';
-import { UserProfileContainer } from '../UserProfileContainer';
+
+const NotFoundContainer = React.lazy(() => import('../NotFoundContainer').PostContainer);
+const PostContainer = React.lazy(() => import('../PostContainer').PostContainer);
+const TermContainer = React.lazy(() => import('../TermContainer').TermContainer);
+const TimelineContainer = React.lazy(() => import('../TimelineContainer').TimelineContainer);
+const UserProfileContainer = React.lazy(() => import('../UserProfileContainer').UserProfileContainer);
+
+const Loading = () => (
+  <Helmet>
+    <title>読込中 - CAwitter</title>
+  </Helmet>
+);
 
 /** @type {React.VFC} */
 const AppContainer = () => {
@@ -32,11 +39,7 @@ const AppContainer = () => {
   const handleRequestCloseModal = React.useCallback(() => setModalType('none'), []);
 
   if (isLoading) {
-    return (
-      <Helmet>
-        <title>読込中 - CAwitter</title>
-      </Helmet>
-    );
+    return <Loading />;
   }
 
   return (
@@ -47,11 +50,13 @@ const AppContainer = () => {
         onRequestOpenPostModal={handleRequestOpenPostModal}
       >
         <Routes>
-          <Route element={<TimelineContainer />} path="/" />
-          <Route element={<UserProfileContainer />} path="/users/:username" />
-          <Route element={<PostContainer />} path="/posts/:postId" />
-          <Route element={<TermContainer />} path="/terms" />
-          <Route element={<NotFoundContainer />} path="*" />
+          <Suspense fallback={<Loading />}>
+            <Route element={<TimelineContainer />} path="/" />
+            <Route element={<UserProfileContainer />} path="/users/:username" />
+            <Route element={<PostContainer />} path="/posts/:postId" />
+            <Route element={<TermContainer />} path="/terms" />
+            <Route element={<NotFoundContainer />} path="*" />
+          </Suspense>
         </Routes>
       </AppPage>
 
