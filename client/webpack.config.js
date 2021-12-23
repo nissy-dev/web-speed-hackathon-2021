21b837172b90cb6b83bf10a57b4a5f81ad0d970b
nissy-dev <nd.12021218@gmail.com>
2021-12-24 00:26:42 +0900

    Fix rendering block

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-// const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin');
+const PreloadWebpackPlugin = require('@vue/preload-webpack-plugin');
 const webpack = require('webpack');
 
 const SRC_PATH = path.resolve(__dirname, './src');
@@ -51,6 +51,7 @@ const config = {
   },
   output: {
     filename: 'scripts/[name].[contenthash].js',
+    publicPath: '/',
     path: DIST_PATH,
   },
   plugins: [
@@ -65,17 +66,16 @@ const config = {
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(SRC_PATH, './index.html'),
-      publicPath: '/',
     }),
-    // new PreloadWebpackPlugin({
-    //   rel: 'preload',
-    //   include: 'all',
-    //   fileBlacklist: [/\.js/],
-    //   as(entry) {
-    //     if (/\.css$/.test(entry)) return 'style';
-    //     return 'script';
-    //   },
-    // }),
+    new PreloadWebpackPlugin({
+      rel: 'preload',
+      include: 'all',
+      fileBlacklist: [/\.js/],
+      as(entry) {
+        if (/\.css$/.test(entry)) return 'style';
+        return 'script';
+      },
+    }),
   ],
   optimization: {
     minimize: true,
